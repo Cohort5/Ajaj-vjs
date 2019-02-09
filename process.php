66c80b74d98ff12b7b4492cfba82f50910495224
Paul Costa <paulcosta.student@careerdevs.com>
2019-02-09 18:40:26 +0000

    minute 59

--- process.php
+++ process.php
@@ -2,6 +2,12 @@
 
 echo 'Processing...';
 
+// check for a POST variable
+if(isset($_POST['name'])){
+    echo 'POST: Your name is '. $_POST['name'];
+}
+
+
 // check for a GET variable
 if(isset($_GET['name'])){
     echo 'GET: Your name is '. $_GET['name'];
